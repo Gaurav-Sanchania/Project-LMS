4f1aa553da1e333aa7afef0fbdbc33d0752fdb9b
Aayush Patel <aayushpatel05054@gmail.com>
2025-03-21 19:08:37 +0530

    Some mainro Changes

--- src/app/dashboard/employeelist/employeelist.component.ts
+++ src/app/dashboard/employeelist/employeelist.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CommomnService } from '../../services/commonService.service';
+import { EmployeeDetailsDialogComponent } from './employee-details-dialog/employee-details-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-employeelist',
@@ -11,11 +13,19 @@ import { CommomnService } from '../../services/commonService.service';
 })
 export class EmployeelistComponent {
   EmployeeList: any = [];
-  constructor(private commonService: CommomnService) {
+  constructor(private commonService: CommomnService, private dialog: MatDialog) {
   }
 
   async ngOnInit() {
     this.EmployeeList = await this.commonService.getEmployeeOnLeave();
     // console.log(this.EmployeeList);
   }
+
+
+    openEmployeeDetails(element: any): void {
+      this.dialog.open(EmployeeDetailsDialogComponent, {
+        width: '500px',
+        data: element
+      });
+    }
 }
