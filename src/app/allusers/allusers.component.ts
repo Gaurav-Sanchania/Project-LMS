50f5841ef654f6823c84ff6ed8ca8c460197bce4
Aayush Patel <aayushpatel05054@gmail.com>
2025-03-21 19:10:03 +0530

    formating

--- src/app/allusers/allusers.component.ts
+++ src/app/allusers/allusers.component.ts
@@ -15,23 +15,23 @@ export class AllusersComponent {
   public userList: any = [];
   dataSource = new MatTableDataSource([]);
   displayedColumns: string[] = ['name', 'department', 'email', 'address', 'phone', 'userType', 'action'];
-  @ViewChild(MatSort) sort!: MatSort; 
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private adminService: AdminService) {}
-  
-    async ngOnInit() {
-      this.userList = await this.adminService.getAllUsers();
-      this.dataSource = new MatTableDataSource(this.userList);
-      // console.log(this.userList);
-    }
+  constructor(private adminService: AdminService) { }
 
-    ngAfterViewChecked() {
-      this.dataSource.sort = this.sort;
-    }
-    editUser(user: any) {
-      console.log("Edit user");
-    }
-    deleteUser(user: any) {
-      console.log("Delete user");
-    }
+  async ngOnInit() {
+    this.userList = await this.adminService.getAllUsers();
+    this.dataSource = new MatTableDataSource(this.userList);
+    // console.log(this.userList);
+  }
+
+  ngAfterViewChecked() {
+    this.dataSource.sort = this.sort;
+  }
+  editUser(user: any) {
+    console.log("Edit user");
+  }
+  deleteUser(user: any) {
+    console.log("Delete user");
+  }
 }
