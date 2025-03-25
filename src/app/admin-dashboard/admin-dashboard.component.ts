79cfcbc335029fad00ced0a6576453ba3810743c
Aayush Patel <aayushpatel05054@gmail.com>
2025-03-25 15:33:04 +0530

    CSS Edjustment.

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -7,16 +7,22 @@ import { PiechartComponent } from "../dashboard/piechart/piechart.component";
 import { ListLeaverequestComponent } from "./list-leaverequest/list-leaverequest.component";
 import { CommomnService } from '../services/commonService.service';
 import { AdminHolidayComponent } from "./admin-holiday/admin-holiday.component";
+import { CalendarComponent } from "../dashboard/calendar/calendar.component";
 
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [EmployeelistComponent, AdminCalendarComponent, PiechartComponent, ListLeaverequestComponent, AdminHolidayComponent],
+  imports: [EmployeelistComponent,  PiechartComponent, ListLeaverequestComponent, AdminHolidayComponent, CalendarComponent],
   providers: [CommomnService],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent {
+  selectedDate: Date = new Date();
+
+  onDateChange(date: Date) {
+    this.selectedDate = date;
+  }
   constructor(private commonService: CommomnService) {
   }
 
